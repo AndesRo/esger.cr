b87d9f593812279a92d50f6485445c80c226aebe
AndresR <129084620+AndesRo@users.noreply.github.com>
2024-06-12 02:51:27 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,67 +1,73 @@
-
-
-// Sello de agua
 const body = document.body;
 body.style.position = 'relative';
 
-const watermark = document.createElement('div');
-watermark.style.position = 'absolute';
-watermark.style.top = 0;
-watermark.style.left = 0;
-watermark.style.width = '100%';
-watermark.style.height = '100%';
-watermark.style.backgroundImage = "url('img/01.png')";
-watermark.style.backgroundSize = 'contain';
-watermark.style.backgroundRepeat = 'no-repeat';
-watermark.style.backgroundPosition = 'center';
-watermark.style.opacity = 0.1;
-watermark.style.zIndex = -1;
-body.appendChild(watermark);
+const watermark1 = createWatermark();
+const watermark2 = createWatermark();
+const watermark3 = createWatermark();
+
+function createWatermark() {
+    const watermark = document.createElement('div');
+    watermark.style.position = 'absolute';
+    watermark.style.top = 0;
+    watermark.style.left = 0;
+    watermark.style.width = '100%';
+    watermark.style.height = '100%';
+    watermark.style.backgroundImage = "url('img/png 2.png')"; // Ruta de la imagen de sello de agua
+    watermark.style.backgroundSize = 'contain';
+    watermark.style.backgroundRepeat = 'no-repeat';
+    watermark.style.backgroundPosition = 'center';
+    watermark.style.opacity = 0.1; // Opacidad del sello de agua (0.1 para que sea sutil)
+    watermark.style.zIndex = -1; // Asegura que el sello de agua esté detrás de otros elementos
+    body.appendChild(watermark);
+    return watermark;
+}
 
 
+// Modal para la galería de imágenes
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("imgModal");
+const captionText = document.getElementById("caption");
 
+function abrirModal(element) {
+    modal.style.display = "block";
+    modalImg.src = element.src;
+    captionText.innerHTML = element.alt;
+}
 
-var span = document.getElementsByClassName("cerrar")[0];
-span.onclick = function() {
+function cerrarModal() {
     modal.style.display = "none";
 }
 
+document.querySelector(".cerrar").onclick = cerrarModal;
+
 // Preguntas Frecuentes
-document.querySelectorAll('.faq-item h3').forEach((question) => {
+document.querySelectorAll('.faq-item h3').forEach(question => {
     question.addEventListener('click', () => {
         const answer = question.nextElementSibling;
-        if (answer.style.display === 'block') {
-            answer.style.display = 'none';
-        } else {
-            answer.style.display = 'block';
-        }
+        answer.style.display = (answer.style.display === 'block') ? 'none' : 'block';
     });
 });
 
-
 // Mostrar la fecha y hora actual
 function mostrarFechaHora() {
-    var fechaHora = new Date();
-    var opciones = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-    var fechaHoraString = fechaHora.toLocaleDateString('es-ES', opciones);
+    const fechaHora = new Date();
+    const opciones = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    const fechaHoraString = fechaHora.toLocaleDateString('es-ES', opciones);
     document.getElementById('fechaHora').innerHTML = fechaHoraString;
 }
 
 setInterval(mostrarFechaHora, 1000);
 mostrarFechaHora();
 
-// script.js envio de formulario
-
+// Envío de formulario
 const $form = document.querySelector('#fs-frm');
 
-$form.addEventListener('submit', handleSubmit);
-
-async function handleSubmit(event) {
+$form.addEventListener('submit', async event => {
     event.preventDefault();
 
-    const formData = new FormData(this);
-    const url = this.action;
-    const method = this.method;
+    const formData = new FormData($form);
+    const url = $form.action;
+    const method = $form.method;
 
     try {
         const response = await fetch(url, {
@@ -73,7 +79,7 @@ async function handleSubmit(event) {
         });
 
         if (response.ok) {
-            this.reset();
+            $form.reset();
             alert('¡Gracias por contactarme!');
         } else {
             throw new Error('Error al enviar el formulario');
@@ -82,30 +88,19 @@ async function handleSubmit(event) {
         console.error('Error:', error);
         alert('Hubo un error al enviar el formulario. Por favor, inténtalo de nuevo más tarde.');
     }
+});
+
+// Función para cerrar el banner
+function closeBanner() {
+    var banner = document.getElementById("banner");
+    banner.style.right = "-300px"; // Oculta el banner moviéndolo fuera de la pantalla
 }
 
+// Muestra el banner después de que se cargue la página
+document.addEventListener("DOMContentLoaded", function() {
+    var banner = document.getElementById("banner");
+    banner.style.right = "20px"; // Muestra el banner desplazándolo desde la derecha
+});
 
-// Obtener el modal
-var modal = document.getElementById('modal');
-
-// Obtener la imagen y el contenedor de la imagen dentro del modal
-var modalImg = document.getElementById("imgModal");
-var captionText = document.getElementById("caption");
-
-// Asignar el evento onclick a todas las imágenes del proyecto
-var images = document.getElementsByClassName("project-img");
-for (var i = 0; i < images.length; i++) {
-    images[i].onclick = function() {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-        captionText.innerHTML = this.alt;
-    }
-}
-
-// Obtener el elemento <span> que cierra el modal
-var span = document.getElementsByClassName("cerrar")[0];
-
-// Cuando el usuario hace clic en <span> (x), cerrar el modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+// Cierra automáticamente el banner después de 3 segundos
+setTimeout(closeBanner, 3000);
